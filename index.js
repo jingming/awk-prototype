d16a02285d2a12b820595bec10e93f852150d75c
Jingming Niu <niu@jingming.ca>
2015-03-23 23:20:21 -0400

    rendering template

--- index.js
+++ index.js
@@ -17,26 +17,30 @@
 
   var Question = mongoose.model('t_questions', questionSchema);
 
-  var question = new Question({
-    data: 'Do you have any siblings?'
-  });
-  question.save(function(result) {
-    console.log(result);
-  });
+  /** dustjs initialization */
+  var dust = require('dustjs-linkedin');
+  dust.helpers = require('dustjs-helpers');
 
   /** App initialization */
   var express = require('express');
+  var consolidate = require('consolidate');
   var app = express();
 
+  app.engine('dust', consolidate.dust);
+  app.set('view engine', 'dust');
+  app.set('views', __dirname + '/dust');
   app.set('port', (process.env.PORT || 5000));
-  app.use(express.static(__dirname + '/public'));
+  app.use(express.static(__dirname));
 
   app.get('/', function (request, response) {
-      response.send("username: " + process.env.MONGO_USERNAME + "\npassword: " + process.env.MONGO_PASSWORD);
+    Question.findOne({}, function(error, question) {
+      console.log(question);
+      response.render('index', { question: question.data });
+    });
   });
 
   app.listen(app.get('port'), function () {
-      console.log("Node app is running at localhost:" + app.get('port'));
+    console.log("Node app is running at localhost:" + app.get('port'));
   });
 
 }());
