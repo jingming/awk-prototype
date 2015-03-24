f5e15968cc3d3b958514a12fc44d6d978c0dc602
Jingming Niu <niu@jingming.ca>
2015-03-23 21:36:16 -0400

    working mongodb connection

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 prototype for awk application
+
+setup notes:
+    heroku instance started using https://devcenter.heroku.com/articles/getting-started-with-nodejs
+    mongodb setup using http://adrianmejia.com/blog/2014/10/01/creating-a-restful-api-tutorial-with-nodejs-and-mongodb/
--- index.js
+++ index.js
@@ -1,13 +1,42 @@
-var express = require('express');
-var app = express();
+/**
+ * awk application initialization
+ *
+ * @author Jingming Niu
+ * @since  2015-03-23
+ */
+(function() {
 
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
+  /** db initialization */
+  var mongoose = require('mongoose');
+  mongoose.connect('mongodb://' + process.env.MONGO_USERNAME + ':' + process.env.MONGO_PASSWORD + '@' + process.env.MONGO_URL);
 
-app.get('/', function(request, response) {
-  response.send("Hello World!");
-});
+  var questionSchema = new mongoose.Schema({
+    data: String,
+    updated_at: { type: Date, default: Date.now }
+  });
 
-app.listen(app.get('port'), function() {
-  console.log("Node appy is running at localhost:" + app.get('port'));
-});
+  var Question = mongoose.model('t_questions', questionSchema);
+
+  var question = new Question({
+    data: 'Do you have any siblings?'
+  });
+  question.save(function(result) {
+    console.log(result);
+  });
+
+  /** App initialization */
+  var express = require('express');
+  var app = express();
+
+  app.set('port', (process.env.PORT || 5000));
+  app.use(express.static(__dirname + '/public'));
+
+  app.get('/', function (request, response) {
+      response.send("username: " + process.env.MONGO_USERNAME + "\npassword: " + process.env.MONGO_PASSWORD);
+  });
+
+  app.listen(app.get('port'), function () {
+      console.log("Node app is running at localhost:" + app.get('port'));
+  });
+
+}());
