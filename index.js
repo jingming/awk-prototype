e55bae37d38241309fcef92b661db07c3c8870c1
Jingming Niu <niu@jingming.ca>
2015-03-23 23:34:33 -0400

    some logs

--- index.js
+++ index.js
@@ -10,10 +10,14 @@
   var mongoose = require('mongoose');
   mongoose.connect('mongodb://' + process.env.MONGO_USERNAME + ':' + process.env.MONGO_PASSWORD + '@' + process.env.MONGO_URL);
 
+  console.log("init: successfully connected to mongodb");
+
   var questionSchema = new mongoose.Schema({
     data: String,
     updated_at: { type: Date, default: Date.now }
   });
+
+  console.log("init: successfully created schema");
 
   var Question = mongoose.model('t_questions', questionSchema);
 
@@ -21,10 +25,14 @@
   var dust = require('dustjs-linkedin');
   dust.helpers = require('dustjs-helpers');
 
+  console.log("init: successfully loaded dust");
+
   /** App initialization */
   var express = require('express');
   var consolidate = require('consolidate');
   var app = express();
+
+  console.log("init: successfully created express instance");
 
   app.engine('dust', consolidate.dust);
   app.set('view engine', 'dust');
@@ -32,12 +40,16 @@
   app.set('port', (process.env.PORT || 5000));
   app.use(express.static(__dirname));
 
+  console.log("init: successfully set initialization requirements");
+
   app.get('/', function (request, response) {
     Question.findOne({}, function(error, question) {
       console.log(question);
       response.render('index', { question: question.data });
     });
   });
+
+  console.log("init: loading application");
 
   app.listen(app.get('port'), function () {
     console.log("Node app is running at localhost:" + app.get('port'));
